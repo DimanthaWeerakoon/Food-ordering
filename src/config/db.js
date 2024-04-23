3f295f13e6fbe9b42e98ee315ff4f01262f00930
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-04-23 21:53:19 +0530

    User model created and db connected

--- src/config/db.js
+++ src/config/db.js
@@ -4,6 +4,8 @@ const { default: mongoose } = require("mongoose");
 
 const mongodbUrl = process.env.DATABASE_URL;
 
-async function cennectDB() {
+async function connectDB() {
   return mongoose.connect(mongodbUrl);
 }
+
+module.exports = connectDB;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const homeRouter = require("./routers/homeRoutes");
 const app = express();
 
 //Middleware
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use("/", homeRouter);
 
 module.exports = { app };
