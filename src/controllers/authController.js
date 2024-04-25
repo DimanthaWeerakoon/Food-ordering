be0b0daca0ed478ca4ed83cfc32ff879f033bead
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-04-25 15:13:38 +0530

    authcontroller created with login and register jwt

---
+++ src/controllers/authController.js
@@ -0,0 +1,30 @@
+const { generateToken } = require("../config/jwtProvider");
+const userService = require("../services/userService");
+const bcrypt = require("bcrypt");
+
+const register = async (req, res) => {
+  try {
+    const user = await userService.createUser(req.body);
+    const jwt = await generateToken(user._id);
+    // await cartService.createCart(user);
+    return res.status(201).send({ jwt, message: "Registration successful" });
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+};
+
+const login = async (req, res) => {
+  const { password, email } = req.body;
+  try {
+    const user = await userService.getUserByEmail(email);
+    const isPasswordValid = bcrypt.compare(password, user.password);
+    if (user) {
+      if (isPasswordValid) {
+        const jwt = await generateToken(user._id);
+        return res.status(200).send({ jwt, message: "Login success" });
+      }
+      return res.status(401).send({ message: "Incorrect password" });
+    }
+    return res.status(404).send({ message: "User not found" });
+  } catch (error) {}
+};
